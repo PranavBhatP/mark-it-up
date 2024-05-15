4a967911cb0e0771f9e279e4ecb5f86080781f64
Pranav Bhat <pranavbhat2004@gmail.com>
2024-05-15 15:05:02 +0530

    fixed secondary bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Editor from './components/Editor';
 import Home from './components/Home';
 import { Routes , Route } from 'react-router-dom';
